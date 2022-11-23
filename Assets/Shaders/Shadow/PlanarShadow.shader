1d7284ee6a73dfc54c59bbafdcb6aa7947708a59
lijiayun.273 <lijiayun.273@bytedance.com>
2022-11-23 19:08:40 +0800

    [add] PlanarShadow

---
+++ Assets/Shaders/Shadow/PlanarShadow.shader
@@ -0,0 +1,71 @@
+Shader "Picoverse/PlanarShadow"
+{
+    Properties
+    {
+        _ShadowColor("Shadow Color", Color) = (0, 0, 0, 1)
+        _ShadowPlaneNormal("Shadow Plane Normal(xyz), Bias(w)", Vector) = (0, 1, 0, 0)
+        _ShadowPlanePosition("Shadow Plane Position(xyz)", Vector) = (0, 0, 0, 0)
+        _ShadowFadeScale("Shadow Fade Scale", Float) = 0
+        
+        [Toggle(_HEIGHTMAP)]  _EnableHeightMap("Enable Height", Float) = 0
+        _HeightMap("Height Map", 2D) = "black" {}
+        _MaxHeight("Max Height", Range(0, 10)) = 1.0
+        
+        [Toggle(_VERTICALOFFSET)]  _VerticalOffset("Vertical Offset", Float) = 0
+        _ProjectDirXZScale("_ProjectDirXZScale", Range(0.0, 4.0)) = 0
+        
+        _ShadowStencil("Planar Shadow Stencil Value", Float) = 15
+    }
+
+    SubShader
+    {
+        Tags {"RenderType" = "Transparent" "IgnoreProjector" = "True" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+
+        Pass
+        {
+            Name "PlanarShadow"
+            Tags{"LightMode" = "UniversalForward"}
+
+            Blend SrcAlpha OneMinusSrcAlpha
+            Zwrite Off
+            Cull Front
+            Stencil 
+            {
+                Ref [_ShadowStencil]
+                Comp NotEqual
+                Pass Replace
+                Fail Keep
+            }
+            
+            // Sets the depth offset for this geometry so that the GPU draws this geometry closer to the camera
+            // You would typically do this to avoid z-fighting
+            Offset -1, -1
+
+            HLSLPROGRAM
+
+            #pragma multi_compile_vertex _ _HEIGHTMAP
+            #pragma multi_compile_vertex _ _VERTICALOFFSET
+           
+            #pragma vertex PlanarShadowVertex
+            #pragma fragment PlanarShadowFragment
+            
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            
+            CBUFFER_START(UnityPerMaterial)
+                float4 _HeightMap_ST;
+                half4 _ShadowColor;
+                float4 _ShadowPlaneNormal;
+                float4 _ShadowPlanePosition;
+                float _ShadowFadeScale;
+                float _ProjectDirXZScale;
+            CBUFFER_END
+
+            //float4 _MainLightPosition;
+            
+            #include "PlanarShadowPass.hlsl"
+            
+            ENDHLSL
+        }
+    }
+}
---
+++ Assets/Shaders/Shadow/PlanarShadowPass.hlsl
@@ -0,0 +1,112 @@
+#ifndef PICOVERSE_PLANAR_SHADOW_PASS_INCLUDED
+#define PICOVERSE_PLANAR_SHADOW_PASS_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+TEXTURE2D(_HeightMap); SAMPLER(sampler_HeightMap);
+
+struct Attributes
+{
+    float4 positionOS   : POSITION;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+    half4 shadowColor   : TEXCOORD0;
+    float4 positionCS   : SV_POSITION;
+};
+
+float3 GetShadowPositionWS(float3 positionOS, float offset)
+{
+    float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+    
+    float3 shadowProjDir = _MainLightPosition.xyz;
+
+    float3 planePositionWS = _ShadowPlanePosition.xyz;
+    float3 planeNormalWS = _ShadowPlaneNormal.xyz;
+    float normalBias = _ShadowPlaneNormal.w;
+
+    planePositionWS += offset * planeNormalWS;
+    
+    float d1 = dot(shadowProjDir, planeNormalWS);
+    float d2 = dot(planePositionWS - positionWS, planeNormalWS);
+    positionWS = positionWS + shadowProjDir * (d2 /d1);
+    
+    return planeNormalWS * normalBias.xxx + positionWS;
+}
+
+half GetShadowFade(float3 positionWS)
+{
+    float3 pivot = float3(GetObjectToWorldMatrix()._14_24_34);
+    float3 pivotToShadow = positionWS - pivot;
+    //pivotToShadow.y = 0;
+    
+    float fadeScale = _ShadowFadeScale;
+    float distanceFade = sqrt(dot(pivotToShadow, pivotToShadow)) * fadeScale;
+    
+    float fade = smoothstep(1, 0, saturate(distanceFade));
+    return fade;
+}
+
+float GetHeight(float3 positionWS)
+{
+    float offset = 0;
+#if defined(_HEIGHTMAP)
+    float2 uv = positionWS.xz * _HeightMap_ST.xy + _HeightMap_ST.zw;
+    float height = _HeightMap.SampleLevel(sampler_HeightMap, uv, 0).x;
+    return height * 0.5;
+    /*
+    if (height > 0.75) offset += 0.5;
+    else if (height > 0.7) offset += 0.25;
+    else if (height > 0.55) offset += 0.10;
+    else offset += 0; */
+#endif   
+    return offset;
+}
+
+float3 ApplyHeightOffset(float3 pos, float offset)
+{
+#ifndef _VERTICALOFFSET
+    float3 dir = _MainLightPosition.xyz;
+    float delta = offset / dot(float3(0.0, 1.0, 0.0), dir);
+    return pos + delta * dir;
+#else
+    float3 dir = _MainLightPosition.xyz;
+    float3 finalOffset = float3(0.0, offset, 0.0);
+    finalOffset += _ProjectDirXZScale * float3(dir.x, 0, dir.z);
+    return pos + finalOffset;
+#endif
+}
+
+Varyings PlanarShadowVertex(Attributes input)
+{
+    Varyings output;
+    UNITY_SETUP_INSTANCE_ID(input);
+
+#if defined(_HEIGHTMAP)
+    float planeHeight = GetHeight(GetObjectToWorldMatrix()._14_24_34);
+    float3 positionWS = GetShadowPositionWS(input.positionOS.xyz, planeHeight);
+
+    float shadowHeight = GetHeight(positionWS);
+    positionWS = ApplyHeightOffset(positionWS, shadowHeight - planeHeight);
+    
+#else
+    float3 positionWS = GetShadowPositionWS(input.positionOS.xyz, 0.0);
+#endif
+    
+    output.positionCS = TransformWorldToHClip(positionWS);
+    output.shadowColor = _ShadowColor;
+    output.shadowColor.a *= GetShadowFade(positionWS);
+    
+    return output;
+}
+
+half4 PlanarShadowFragment(Varyings input) : SV_TARGET
+{
+    // TODO: Alpha Clipping
+    half4 color = input.shadowColor;
+    return color;
+}
+
+#endif
